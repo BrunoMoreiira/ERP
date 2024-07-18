8015ffc8f9992cb4ff0298c459b295ecdd56e824
BrunoMoreiira <bruno.firmo09@gmail.com>
2024-07-18 20:48:41 -0300

    add endpoint

--- accounts/views/base.py
+++ accounts/views/base.py
@@ -1,10 +1,11 @@
 from rest_framework.views import APIView
-from companies.models import Enterprise
-
+from rest_framework.exceptions import APIException
+from companies.models import Employee, Enterprise
+from accounts.models import User_Groups, GroupPermission
 
 class Base(APIView):
-    def get_enterprise_user(self,user_id):
-        enterprise= {
+    def get_enterprise_user(self, user_id):
+        enterprise = {
             "is_owner": False,
             "permissions": []
         }
@@ -12,5 +13,31 @@ class Base(APIView):
 
         if enterprise['is_owner']:
             return enterprise
+        return None
+
+    def get_employee_and_groups(self, user_id):
+        employee = Employee.objects.filter(user_id=user_id).first()
+
+        if not employee:
+            raise APIException("Este usuário não é um funcionário")
+
+        enterprise = {
+            "is_owner": False,
+            "permissions": []
+        }
+
+        groups = User_Groups.objects.filter(user_id=user_id).all()
         
-    # Permissions, Get Employee
+        for g in groups:
+            group = g.group 
+            
+            permissions = GroupPermission.objects.filter(group_id=group.id).all()
+
+            for p in permissions:
+                enterprise['permissions'].append({
+                    "id": p.permission.id,
+                    "label": p.permission.name,
+                    "codename": p.permission.codename,
+                })
+
+        return enterprise
